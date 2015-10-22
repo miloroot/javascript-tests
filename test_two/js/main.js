ed8aa86f5b23758d4e3267bc0992aac57e974709
Milo <milonice@hotmail.com>
2015-10-22 13:56:42 +0200

    Add: function being overridden depending on true or false state of variable setter. Code in vanilla jQuery. Now to change the code to the design pattern Revealing Modular Pattern.

--- test_two/js/main.js
+++ test_two/js/main.js
@@ -1,5 +1,26 @@
-var TestModule = ( function( window, undefined ) {
+$( function() {
 
-  
+  var $setTrueButton = $( '.set-true' ),
+      $setFalseButton = $( '.set-false' );
 
-} )( window );
+  var setter = undefined;
+
+  $setTrueButton.on( 'click', function() {
+    setter = true;
+    console.log( "variable setter is now: ", setter );
+    changerFunc = function() {
+      console.log( "poop alarm is for real" );
+    }
+    changerFunc();
+  });
+
+  $setFalseButton.on( 'click', function() {
+    setter = false;
+    console.log( "variable setter is now: ", setter );
+    changerFunc = function() {
+      console.log( "false poop alarm" )
+    }
+    changerFunc();
+  });
+
+});
